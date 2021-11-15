7f509f739c16e2aa2d008b0aa63a51f44da38571
Jeremy Davis <jeremy@turnkeylinux.org>
2021-11-15 21:29:56 +1100

    Tweaks - nearly there...

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ BUILD_DIR=modsec_build
 mkdir -p $BUILD_DIR
 
 # download and unpack source package 
-NGINX_VER=$(apt-cache policy nginx | grep Installed: | sed "s|^.*: \([1-9]\.[0-9].*\)|\1|")
-URL=http://deb.debian.org/debian/pool/main/n/nginx
-wget $URL/nginx_${NGINX_VER}.dsc
+NGINX_VER=$(apt-cache policy nginx-light | grep Installed: | sed "s|^.*: \([1-9]\.[0-9].*\)|\1|")
+URL=https://deb.debian.org/debian/pool/main/n/nginx
+curl -o nginx_${NGINX_VER}.dsc $URL/nginx_${NGINX_VER}.dsc
 FILES=$(grep "Files:" -A3 nginx_${NGINX_VER}.dsc | tail -3 | sed "s|.*nginx|nginx|")
 for dl_file in $FILES; do
-    wget $URL/$dl_file
+    curl -o $dl_file $URL/$dl_file
 done
-dpkg-source -x nginx_${NGINX_VER}.ds
+dpkg-source -x nginx_${NGINX_VER}.dsc
 
 # collect default Debian build options and tweak as required
 OPTS=$(/usr/sbin/nginx -V 2>&1 | \
@@ -33,21 +33,20 @@ cd nginx-1.*
 ./configure --with-cc-opt="$CC_OPTS" --with-ld-opt="$LD_OPTS" $OPTS \
     --add-dynamic-module=../ModSecurity-nginx
 make modules
-mkdir -p /etc/nginx/{modules,modsec}
-cp objs/ngx_http_modsecurity_module.so /etc/nginx/modules/
-echo "load_module /etc/nginx/modules/ngx_http_modsecurity_module.so;" \
-    > /etc/nginx/modules-available/modsecurity.conf
-ln -sf /etc/nginx/modules-available/modsecurity.conf /etc/nginx/modules-enabled/
-curl -o /etc/nginx/modsec/modsecurity.conf \
-    https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended
-curl -o /etc/nginx/modsec/unicode.mapping \
-    https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/unicode.mapping
-cat > /etc/nginx/modsec/main.conf <<EOF
-Include "/etc/nginx/modsec/modsecurity.conf"
-SecRule ARGS:testparam "@contains test" "id:1234,deny,status:403"
-EOF
-cat > /etc/nginx/include/modsecurity.conf <<EOF
+cd ..
+mkdir -p etc/nginx/{modules-available,modsec}
+cp nginx-1.*/objs/ngx_http_modsecurity_module.so etc/nginx/modules-available/
+cat > etc/nginx/modules-available/modsecurity.conf <<EOF
+load_module /etc/nginx/modules-available/ngx_http_modsecurity_module.so;
 modsecurity on;
 modsecurity_rules_file /etc/nginx/modsec/main.conf;
 EOF
-
+#ln -sf /etc/nginx/modules-available/modsecurity.conf /etc/nginx/modules-enabled/
+curl -o etc/nginx/modsec/modsecurity.conf \
+    https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended
+curl -o etc/nginx/modsec/unicode.mapping \
+    https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/unicode.mapping
+cat > etc/nginx/modsec/main.conf <<EOF
+Include "/etc/nginx/modsec/modsecurity.conf"
+SecRule ARGS:testparam "@contains test" "id:1234,deny,status:403"
+EOF
